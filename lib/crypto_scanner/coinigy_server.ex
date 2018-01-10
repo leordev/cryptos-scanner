bfafa8c34ae74755573b5a33bb1405b74fa9b583
Leo Ribeiro <leordev@gmail.com>
2018-01-10 09:17:57 -0500

    add bitfinex

--- lib/crypto_scanner/coinigy_server.ex
+++ lib/crypto_scanner/coinigy_server.ex
@@ -114,6 +114,9 @@ defmodule CryptoScanner.CoinigyServer do
       GenServer.call(:coinigy, {:subscribe_to_channels, "HITB", "USD"})
       GenServer.call(:coinigy, {:subscribe_to_channels, "HITB", "ETH"})
       GenServer.call(:coinigy, {:subscribe_to_channels, "BINA", "BTC"})
+      GenServer.call(:coinigy, {:subscribe_to_channels, "BITF", "BTC"})
+      GenServer.call(:coinigy, {:subscribe_to_channels, "BITF", "ETH"})
+      GenServer.call(:coinigy, {:subscribe_to_channels, "BITF", "USD"})
     end
   end
 
---
+++ rel/ctb/lib/cors_plug-1.5.0/ebin/cors_plug.app
@@ -0,0 +1,6 @@
+{application,cors_plug,
+             [{description,"An elixir plug that adds CORS headers to requests and responds to\npreflight requests (OPTIONS)\n"},
+              {modules,['Elixir.CORSPlug']},
+              {registered,[]},
+              {vsn,"1.5.0"},
+              {applications,[kernel,stdlib,elixir,logger]}]}.
