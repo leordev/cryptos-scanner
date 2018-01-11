41f2f8218f91c23f81b0349e665475b20e655c68
Leo Ribeiro <leordev@gmail.com>
2018-01-10 20:26:50 -0500

    new deploy test

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CryptoScanner.Mixfile do
   def project do
     [
       app: :crypto_scanner,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
